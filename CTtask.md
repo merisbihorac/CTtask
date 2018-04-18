d73f15e012e15c4a0652db93aa6ea526576e1e8a
Meris Bihorac <32592629+merisbihorac@users.noreply.github.com>
2018-04-18 11:35:36 +0200

    Add files via upload
    commit

---
+++ CTtask.md
@@ -0,0 +1,26 @@
+### 
+# This is Comtrade testing task for Junior Developer
+
+##1.       Following good development practices, write a program which can be called from the command line and has this functionality:
+
+ 
+
+-          Connects to this URL (https://jsonplaceholder.typicode.com/users) to fetch and parse JSon data.
+
+-          Counts users by their email domain (i.e. .net, .org. com, etc.)
+
+-          Prints out top 10 results in format “domain - count”
+
+##2.       Also please share your thoughts on following, (there is no unique right answer):
+
+ 
+
+-          Which functional / non-functional flaws can you identify from this specification?
+
+-          Considering the whole system, which failure points can you identify?
+
+-          How would you notify the user if there are issues with execution of the application?
+
+-          How would you measure performance of this application?
+
+-          How would you stress-test this application?
---
+++ ProgramSolution/CTfun/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;//Used for JSON parsing
+
+namespace CTfun
+{
+
+    class Program
+    {
+
+        //json2csharp converting
+        public class Geo
+        {
+            public string lat { get; set; }
+            public string lng { get; set; }
+        }
+
+        public class Address
+        {
+            public string street { get; set; }
+            public string suite { get; set; }
+            public string city { get; set; }
+            public string zipcode { get; set; }
+            public Geo geo { get; set; }
+        }
+
+        public class Company
+        {
+            public string name { get; set; }
+            public string catchPhrase { get; set; }
+            public string bs { get; set; }
+        }
+
+        public class Users
+        {
+            public int id { get; set; }
+            public string name { get; set; }
+            public string username { get; set; }
+            public string email { get; set; }
+            public Address address { get; set; }
+            public string phone { get; set; }
+            public string website { get; set; }
+            public Company company { get; set; }
+        }
+        static void Main(string[] args)
+        {
+
+            var client = new WebClient();
+            var content = client.DownloadString("https://jsonplaceholder.typicode.com/users");//JSON content
+
+            Users[] result=JsonConvert.DeserializeObject<Users[]>(content);//deserialization and insertion in array
+            
+            //Console.WriteLine(result[0].name); //Showing name atribute from firs object in array
+            //Console.ReadLine(); //Hold on
+
+            /* email of users
+            foreach (Users i in result)
+            {
+                Console.WriteLine(i.email);
+            }
+            Console.ReadLine();
+            */
+            //brojaci domena
+            int biz = 0;
+            int tv = 0;
+            int net = 0;
+            int org = 0;
+            int ca = 0;
+            int info = 0;
+            int me = 0;
+            int io = 0;
+
+            foreach (Users i in result)
+            {
+
+                if (i.email.Contains(".biz")) { biz++; }
+                else if (i.email.Contains(".biz")) { biz++; }
+                else if (i.email.Contains(".tv")) { tv++; }
+                else if (i.email.Contains(".net")) { net++; }
+                else if (i.email.Contains(".org")) { org++; }
+                else if (i.email.Contains(".ca")) { ca++; }
+                else if (i.email.Contains(".info")) { info++; }
+                else if (i.email.Contains(".me")) { me++; }
+                else if (i.email.Contains(".io")) { io++; }
+            }
+
+            Console.WriteLine("Number of users with email domains:\n");
+
+            Console.WriteLine("Users with domain .biz : " +biz);
+            Console.WriteLine("Users with domain .tv : " + tv);
+            Console.WriteLine("Users with domain .net : " + net);
+            Console.WriteLine("Users with domain .org : " + org);
+            Console.WriteLine("Users with domain .ca : " + ca);
+            Console.WriteLine("Users with domain .info : " + info);
+            Console.WriteLine("Users with domain .me : " + me);
+            Console.WriteLine("Users with domain .io : " + io);
+
+            Console.WriteLine("\nPress ENTER to finish!");
+            Console.ReadLine();
+
+        }
+
+    }
+}
